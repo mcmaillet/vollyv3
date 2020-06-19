8ea9ac907e93a0a95d6160bb8a8c1575da51b825
mcmaillet <maillet.mark@gmail.com>
2020-06-18 22:54:43 -0600

    wip

---
+++ VollyV3/Controllers/OrganizationAdministrator/ApplicationsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VollyV3.Models;
+
+namespace VollyV3.Controllers.OrganizationAdministrator
+{
+    [Authorize(Roles = "OrganizationAdministrator", Policy = "IsConfigured")]
+    public class ApplicationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<VollyV3User> _userManager;
+
+        public ApplicationsController(
+           ApplicationDbContext context,
+           UserManager<VollyV3User> userManager
+           )
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            var applications = _context.Applications
+                .Include(x => x.Opportunity)
+                .ThenInclude(x => x.CreatedBy)
+                .Include(x => x.Occurrence)
+                .Where(x => x.Opportunity.CreatedBy.User == user)
+                .OrderBy(x => x.Opportunity.Id)
+                .ToList();
+
+            return View(applications);
+        }
+    }
+}
