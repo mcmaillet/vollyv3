257e2e6d68b3af9ef54e671e8ffc80fa789e9b82
Mark Maillet <maillet.mark@gmail.com>
2020-12-19 18:45:52 -0400

    add platform application control

--- VollyV3/Controllers/PlatformAdministrator/PlatformApplicationsController.cs
+++ VollyV3/Controllers/PlatformAdministrator/PlatformApplicationsController.cs
@@ -1,42 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using VollyV3.Areas.Identity;
-using VollyV3.Data;
 using VollyV3.Models;
 
-namespace VollyV3.Controllers.OrganizationAdministrator
+namespace VollyV3.Controllers.PlatformAdministrator
 {
-    [Authorize(Roles = nameof(Role.OrganizationAdministrator), Policy = "IsConfigured")]
-    public class ApplicationsController : Controller
+    [Authorize(Roles = nameof(Role.PlatformAdministrator))]
+    public class PlatformApplicationsController : Controller
     {
         private readonly ApplicationDbContext _context;
-        private readonly UserManager<VollyV3User> _userManager;
 
-        public ApplicationsController(
-           ApplicationDbContext context,
-           UserManager<VollyV3User> userManager
-           )
+        public PlatformApplicationsController(ApplicationDbContext context)
         {
             _context = context;
-            _userManager = userManager;
         }
-
-        public async Task<IActionResult> IndexAsync()
+        public IActionResult Index()
         {
-            var user = await _userManager.GetUserAsync(HttpContext.User);
             var applications = _context.Applications
                 .Include(x => x.Opportunity)
                 .ThenInclude(x => x.CreatedBy)
+                .ThenInclude(x => x.Organization)
                 .Include(x => x.Occurrence)
-                .Where(x => x.Opportunity.CreatedBy.User == user)
-                .OrderBy(x => x.Opportunity.Id)
+                .OrderByDescending(x => x.Opportunity.Id)
                 .ToList();
 
             return View(applications);
@@ -47,47 +39,39 @@ namespace VollyV3.Controllers.OrganizationAdministrator
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpGet]
-        public async Task<IActionResult> DeleteAsync(int id)
+        public IActionResult Delete(int id)
         {
+
             var application = _context.Applications
                 .Include(x => x.Opportunity)
                 .ThenInclude(x => x.CreatedBy)
+                .ThenInclude(x => x.Organization)
                 .Include(x => x.Occurrence)
                 .Where(x => x.Id == id)
                 .SingleOrDefault();
+
             if (application == null)
             {
                 TempData["Messages"] = "Application not found.";
                 return RedirectToAction(nameof(Index));
             }
-            var user = await _userManager.GetUserAsync(HttpContext.User);
-            if (application.Opportunity.CreatedBy.User != user)
-            {
-                TempData["Messages"] = "You are not the owner of that opportunity. You cannot modify applications.";
-                return RedirectToAction(nameof(Index));
-            }
+
             return View(application);
         }
         [HttpPost]
-        public async Task<IActionResult> DeleteAsync(int id, IFormCollection form)
+        public IActionResult Delete(int id, IFormCollection form)
         {
+
             var application = _context.Applications
-                .Include(x => x.Opportunity)
-                .ThenInclude(x => x.CreatedBy)
-                .Include(x => x.Occurrence)
                 .Where(x => x.Id == id)
                 .SingleOrDefault();
+
             if (application == null)
             {
                 TempData["Messages"] = "Application not found.";
                 return RedirectToAction(nameof(Index));
             }
-            var user = await _userManager.GetUserAsync(HttpContext.User);
-            if (application.Opportunity.CreatedBy.User != user)
-            {
-                TempData["Messages"] = "You are not the owner of that opportunity. You cannot modify applications.";
-                return RedirectToAction(nameof(Index));
-            }
+
             _context.Remove(application);
             _context.SaveChanges();
             TempData["Messages"] = $"Application deleted.";
