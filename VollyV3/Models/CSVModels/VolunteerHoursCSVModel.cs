45cb02d520b012be53c2f4ea067954b76ea3d92a
Mark Maillet <mark.maillet@hedhome.com>
2021-01-24 18:23:13 -0700

    browse min and vol hours

--- VollyV3/Models/CSVModels/VolunteerHoursCSVModel.cs
+++ VollyV3/Models/CSVModels/VolunteerHoursCSVModel.cs
@@ -6,8 +6,6 @@ namespace VollyV3.Models.CSVModels
 {
     public class VolunteerHoursCSVModel
     {
-        public string Company { get; set; }
-        public string CompanyCode { get; set; }
         public string User { get; set; }
         public string OrganizationName { get; set; }
         public string OpportunityName { get; set; }
@@ -34,8 +32,6 @@ namespace VollyV3.Models.CSVModels
     {
         public VolunteerHoursCSVModelMap()
         {
-            Map(m => m.Company).Name("Company");
-            Map(m => m.CompanyCode).Name("CompanyCode");
             Map(m => m.User).Name("User");
             Map(m => m.OpportunityName).Name("OpportunityName");
             Map(m => m.OrganizationName).Name("OrganizationName");
