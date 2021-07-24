4f3727ab6e2eb9374ef22dd5ae69540dfb93b4a5
Mark Maillet <mark.maillet@hedhome.com>
2021-07-24 00:09:38 -0600

    infinite scroll

--- VollyV3/wwwroot/js/home.js
+++ VollyV3/wwwroot/js/home.js
@@ -1,5 +1,9 @@
 ﻿var map;
 var markers = [];
+var currentPage = 1;
+var currentLimit = 12;
+var currentOpportunityType;
+var currentTotalCount = 0;
 
 function addOpportunityMarker(opportunity) {
     if (opportunity.latitude && opportunity.longitude) {
@@ -212,16 +216,17 @@ function setMapOnAll(map) {
 
 $(".opportunityType").click(function () {
     var opportunityType = parseInt($(this).attr('value'));
-    filter(opportunityType);
+    filter(opportunityType, 1);
 });
 
-function filter(opportunityType) {
+function filter(opportunityType, page = 1) {
+    currentPage = page;
+    var opportunityTypeChanged = currentOpportunityType != opportunityType;
+    currentOpportunityType = opportunityType;
     var data = {
-        "CategoryIds": null,
-        "CauseIds": null,
-        "OrganizationIds": null,
         "OpportunityType": opportunityType,
-        "Dates": null,
+        "Page": page,
+        "Limit": currentLimit,
         "Sort": 1
     };
 
@@ -231,12 +236,18 @@ function filter(opportunityType) {
         url: '/api/Search/Opportunities',
         data: JSON.stringify(data),
         dataType: "json",
-        success: function (opportunities) {
-            clearOpportunities();
+        success: function (data) {
+            var opportunities = data.opportunities;
+            currentTotalCount = data.totalCount;
+            if (opportunityTypeChanged) {
+                clearOpportunities();
+            }
             $('#loader').show();
             if (opportunities.length === 0) {
                 $('#loader').hide();
-                $('#nothingFoundAlert').show();
+                if (opportunityTypeChanged) {
+                    $('#nothingFoundAlert').show();
+                }
             } else {
                 $('#nothingFoundAlert').hide();
                 addOpportunityMarkers(opportunities);
@@ -269,7 +280,27 @@ function toggleFilterVisibility(filterid) {
         $("#filter-wrapper").addClass("filter-wrapper-show")
     }
 }
+
 (function () {
+    window.addEventListener('scroll', () => {
+        const {
+            scrollTop,
+            scrollHeight,
+            clientHeight
+        } = document.documentElement;
+
+        const hasMoreResults = (page) => {
+            const startIndex = (page - 1) * currentLimit + 1;
+            return currentTotalCount === 0 || startIndex < currentTotalCount;
+        };
+
+        if (scrollTop + clientHeight >= scrollHeight - 5 && hasMoreResults(currentPage)) {
+            currentPage++;
+            filter(currentOpportunityType, currentPage);
+        }
+    }, {
+        passive: true
+    })
     $("#toggleMap").click(function () {
         var dataShow = parseInt($('#toggleMap').attr('data-show'));
         if (dataShow === 1) {
